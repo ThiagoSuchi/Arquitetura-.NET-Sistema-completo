139989ed0c17e60c7832d7131df3e1699b15f78a
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-06-10 17:05:05 -0400

    feat: cria estrutura e arquivos de Bootstraps

---
+++ 1 - Backend/1.4 - API/AppProject.Core.API/Bootstraps/Bootstrap.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Reflection;
+
+namespace AppProject.Core.API.Bootstraps;
+
+public static class Bootstrap
+{
+  public static WebApplicationBuilder AddApiServices(this WebApplicationBuilder builder)
+  {
+    var mvcBuilder = builder.Services.AddControllers();
+
+    ConfigureControllers(mvcBuilder);
+
+    return builder;
+  }
+
+  public static WebApplication UseApiPipeline(this WebApplication app)
+  {
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.MapControllers();
+
+    return app;
+  }
+
+  // Esse método percorre sobre outros projetos que possuem controllers, e adiciona esses controllers no próprio projeto
+  private static void ConfigureControllers(IMvcBuilder mvcBuilder)
+  {
+    foreach (var assembly in GetControllerAssemblies())
+    {
+      mvcBuilder.AddApplicationPart(assembly);
+    }
+  }
+
+  private static IEnumerable<Assembly> GetControllerAssemblies() =>
+  [
+    Assembly.Load("AppProject.Core.Controllers.General"),
+  ];
+}
